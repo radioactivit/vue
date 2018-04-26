de1075d6d66833d6a9063bd74f0043015e707ee7
jerome <jerome.chaaban@gmail.com>
2018-04-26 15:49:43 +0200

    router app example

--- src/script.js
+++ src/script.js
@@ -3,36 +3,92 @@ import * as moment from "moment";
 //Vue js
 import Vue from "vue";
 import VueResource from "vue-resource";
+import Vuex from 'vuex'
+import { mapState } from 'vuex'
+import VueRouter from 'vue-router'
 
-import { component as userComponent } from "./userComponentVariation";
+import { component as userComponent } from "./userComponent";
+import { component as userComponentVariation } from "./userComponentVariation";
 
-Vue.component("user-component", userComponent);
-
-//Add vue ressource to vue js (for having http support)
+//Add Vuex and VueResource
 Vue.use(VueResource);
+Vue.use(Vuex);
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+    routes: [
+        { path: '/old', component: userComponent },
+        { path: '/new', component: userComponentVariation },
+        {
+            path: '/user/:id', name: "user", component: {
+                template: '<div>User id = {{ $route.params.id }}</div>'
+            }
+        }
+    ]
+});
+
+//Define components
+Vue.component("user-component", userComponent);
+Vue.component("test-component-in-component", {
+    template: "<div>Subsubsub-component</div>"
+});
+
+const store = new Vuex.Store({
+    state: {
+        count: 0
+    },
+    mutations: {
+        increment(state) {
+            state.count++
+        }
+    }
+});
+
+//array and object destructuring
+let [a, b] = [5, 2];
+let { c, d } = { c: 43, d: 67 };
+
+let myFunction = ({ firstname, name }) => {
+    console.log(firstname, name);
+};
+
+myFunction({ firstname: "luc", name: "loyant", age: 43, sex: "h" });
 
 var app = new Vue({
     el: '#app',
+    router,
+    store,
     data: {
         message: 'Hello Trapil!',
         user: {
+            id: 124,
             firstname: "jerome",
             name: "chaaban",
             age: 18
-        }
+        },
+        classes: ['alert']
     },
     methods: {
         reversedMessageMethod: function (message) {
             return this.message.split('').reverse().join('')
+        },
+        goBack: function () {
+            window.history.length > 1
+                ? this.$router.go(-1)
+                : this.$router.push('/')
+        },
+        increment: function () {
+            debugger;
+            this.$store.commit("increment");
         }
     },
-    computed: {
+    computed: Object.assign({}, mapState(["count"]), {
         // a computed getter
         reversedMessage: function () {
             // `this` points to the vm instance
             return this.message.split('').reverse().join('')
         }
-    },
+    }),
     watch: {
         // whenever question changes, this function will run
         message: function (newMessage, oldMessage) {
@@ -44,34 +100,52 @@ var app = new Vue({
 
 setTimeout(() => {
     app.message = "Thanks Trapil";
+    app.classes.push('alert-primary');
 }, 2000);
 
-(async () => {
+/*(async () => {
     await new Promise((resolve, reject) => resolve(112));
-})();
+})();*/
 
+const users = [
+    { firstname: "luc", name: "loyant", "age": 50, "since": 2002 },
+    { firstname: "aime", name: "broohm", "age": 47, "since": 2005 },
+    { firstname: "david", name: "dellier", "age": 42, "since": 2004 },
+    { firstname: "jerome", name: "chaaban", "age": 26, "since": 2018 }
+];
 
-var app2 = new Vue({
-    el: '#app-2',
-    data: {
-        message: 'Page loaded on ' + new Date().toLocaleString()
-    }
+users.map(aUser => aUser.id = aUser.firstname + aUser.name);
+
+const userRouter = new VueRouter({
+    routes: [
+        {
+            path: "/users/:id",
+            name: "userDetail",
+            component: {
+                template: `<div>Hello {{user.id}}
+                <p v-for='prop in user'>{{prop}}</p>
+                <test-component-in-component></test-component-in-component>
+                </div>`,
+                data: function () {
+                    return {
+                        user: null
+                    }
+                },
+                created: function () {
+                    console.log("CREATED");
+                    setTimeout(() => {
+                        this.user = users.find(user => user.id === this.$route.params.id);
+                    }, 3000);
+                }
+            }
+        }
+    ]
 });
 
-var app3 = new Vue({
-    el: '#app-3',
+var myAppUsersThatCanBeNamedWhatever = new Vue({
+    el: '#appUsers',
+    router: userRouter,
     data: {
-        seen: true
-    }
-});
-
-var app4 = new Vue({
-    el: '#app-4',
-    data: {
-        todos: [
-            { text: 'Vue avec npm' },
-            { text: 'Vue avec webpack' },
-            { text: 'Vue sans file://' }
-        ]
+        users
     }
 });
