0ac8e65a7844efabba88e07c740de93dfbec9fb7
jerome <jerome.chaaban@gmail.com>
2018-04-26 10:06:54 +0200

    Add static files renderer

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
 
+app.use("/static", express.static("bin"))
+
 app.get('/', function (req, res) {
     res.send('Hello World!')
 })
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!')
+app.listen(80, function () {
+    console.log('Listening on port 80!')
 })
